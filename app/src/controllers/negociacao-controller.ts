8dcc764f3e9437b2e0ec5f3d93e62db0940cfc8e
Francisco das Chagas de Oliveira <f.chagas@itinovacao.onmicrosoft.com>
2022-03-15 09:32:00 -0300

    feat: Decorator (@escape, inspect )

--- app/src/controllers/negociacao-controller.ts
+++ app/src/controllers/negociacao-controller.ts
@@ -1,3 +1,4 @@
+import { inspect } from '../decorators/inspect.js';
 import { LogartempoDeExecucao } from '../decorators/logar-tempo-de-execucao.js';
 import { DiasDaSemana } from '../enums/dias-da-semana.js';
 import { Negociacao } from '../models/negociacao.js';
@@ -10,7 +11,8 @@ export class NegociacaoController {
     private inputQuantidade: HTMLInputElement;
     private inputValor: HTMLInputElement;
     private negociacoes = new Negociacoes();
-    private negociacoesView = new NegociacoesView('#negociacoesView', true);
+    //private negociacoesView = new NegociacoesView('#negociacoesView', true);
+    private negociacoesView = new NegociacoesView('#negociacoesView');
     private mensagemView = new MensagemView('#mensagemView');
 
     constructor() {
@@ -23,6 +25,8 @@ export class NegociacaoController {
 
     //Analisando a performance e o tempo de renderização de uma aplicação com typescript
     @LogartempoDeExecucao()
+    //@inspect()
+    @inspect
     public adiciona(): void {
         const negociacao = Negociacao.criaDe(
             this.inputData.value,
--- app/src/views/view.ts
+++ app/src/views/view.ts
@@ -1,31 +1,35 @@
+import { inspect } from "../decorators/inspect.js";
 import { LogartempoDeExecucao } from "../decorators/logar-tempo-de-execucao.js";
 
 export abstract class View<T> {
 
     protected elemento: HTMLElement;
-    private escapar = false;
+    //private escapar = false;
 
-    constructor(seletor: string, escapar?: boolean) {
+    //constructor(seletor: string, escapar?: boolean) {
+    constructor(seletor: string) {
         const elemento = document.querySelector(seletor);
         if (elemento) {
             this.elemento = elemento as HTMLElement;
         } else {
             throw Error(`Seletor ${seletor} não existe no DOM. Verifique`);
         }
-        if (escapar) {
-            this.escapar = escapar;
-        }
+        // if (escapar) {
+        //     this.escapar = escapar;
+        // }
     }
 
     //Analisando a performance e o tempo de renderização de uma aplicação com typescript
     @LogartempoDeExecucao(true)
+    //@inspect()
+    inspect@
     public update(model: T): void {
         let template = this.template(model);
-        if (this.escapar) {
-            template = template
-                .replace(/<script>[\s\S]*?<\/script>/, '');
-        }
-        this.elemento.innerHTML = template;       
+        // if (this.escapar) {
+        //     template = template
+        //         .replace(/<script>[\s\S]*?<\/script>/, '');
+        // }
+        this.elemento.innerHTML = template;
     }
     protected abstract template(model: T): string;
 }
